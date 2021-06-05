247d195d5404f5508d457fad5df807fc20ed1aa4
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2021-06-06 01:10:15 +0200

    Enable utoa

--- cores/tivac/itoa.c
+++ cores/tivac/itoa.c
@@ -121,7 +121,7 @@ extern char* ltoa( long value, char *string, int radix )
   return string;
 }
 
-#if 0
+#if 1
 extern char* utoa( unsigned long value, char *string, int radix )
 {
   return ultoa( value, string, radix ) ;
